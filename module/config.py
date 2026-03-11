9bb89137ea9f16b91ad0c43b2890413b0ac844c5
Carboneinerte <hugo.calligaro@gmail.Com>
2026-03-10 17:18:58 -0700

    restore config.py

---
+++ module/config.py
@@ -0,0 +1,11 @@
+### Config file
+# modify to fit your need then rename the file as "config_local.py". Do not move it.
+
+## Path to the raw Xenium files
+dir_raw = ""
+
+## Path to where you want to save your processed files
+dir_notebook = ''
+
+## Name of your experiment
+# name_dir = ""
